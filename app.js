b451d3796d1178dbc89c985a888e6359a8a4ad5e
kuvvat <kuveyt94.94@gmail.com>
2024-07-27 01:21:26 +0500

    add new folder

--- app.js
+++ app.js
@@ -25,7 +25,6 @@ const server = http.createServer((req, res)=>{
             res.writeHead(200, {'Content-type': 'application/json'})
             res.end(data)
         })
-       
     }
 
     else{
